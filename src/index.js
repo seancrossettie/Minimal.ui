a5f9b6351568e8e9f7be6a79ac3fdeaf78b33102
seancrossettie <seancrossettie@gmail.com>
2021-12-16 16:57:42 -0600

    added more routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import firebaseConfig from "./helpers/apiKeys";
