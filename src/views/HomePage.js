3c13a6493831b24b652585437c400f714efbf2d4
seancrossettie <seancrossettie@gmail.com>
2021-12-16 15:44:12 -0600

    added setup files

---
+++ src/views/HomePage.js
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react";
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div>
+            <Text>Home Page</Text>
+        </div>
+    );
+};
+
+export default HomePage;
