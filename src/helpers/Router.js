1bdc8e45fa7d59bd081da049756b031908782629
seancrossettie <seancrossettie@gmail.com>
2021-12-27 15:57:33 -0600

    added private routing

--- src/helpers/Router.js
+++ src/helpers/Router.js
@@ -1,19 +1,57 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import CategoriesPage from "../views/CategoriesPage";
-import HomePage from "../views/HomePage";
+import firebase from "firebase/compat/app";
 import ItemsPage from "../views/ItemsPage";
 import ProfilePage from "../views/ProfilePage";
+import { HomePage } from "../views/HomePage";
+import LoginPage from "../views/LoginPage";
+import { NotFound } from "../views/NotFound";
 
 function Router({ user, setUser, phoroURL }) {
     return (
         <Routes>
-            <Route path="/" element={<HomePage user={user} setUser={setUser} />} />
-            <Route path="/items" element={<ItemsPage user={user} setUser={setUser} />} />
-            <Route path="/categories" element={<CategoriesPage user={user} setUser={setUser} />} />
-            <Route path="/profile" element={<ProfilePage user={user} setUser={setUser} />} />
+            <Route path="/login" element={<LoginPage user={user} setUser={setUser} />} />
+            <Route 
+                path="/"
+                element={
+                     <PrivateRoute>
+                        <HomePage user={user} setUser={setUser} />
+                    </PrivateRoute>
+                }
+            />
+            <Route 
+                path="/items" 
+                element={
+                    <PrivateRoute>
+                        <ItemsPage user={user} setUser={setUser} />
+                    </PrivateRoute>
+                }>
+            </Route>
+            <Route 
+                path="/categories" 
+                element={
+                    <PrivateRoute>
+                        <CategoriesPage user={user} setUser={setUser} />
+                    </PrivateRoute>
+                }>
+            </Route>
+            <Route 
+                path="/profile" 
+                element={
+                    <PrivateRoute>
+                        <ProfilePage user={user} setUser={setUser} />
+                    </PrivateRoute>
+                 }>
+            </Route>
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
+};
+
+const PrivateRoute = ({ children }) => {
+    const user = firebase.auth().currentUser;
+    return user ? children : <Navigate to="/login" />
 };
 
 export default Router;
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,14 +1,10 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
-import React from "react";
+import { Text } from '@chakra-ui/react'
+import React from 'react'
 
-const HomePage = ({ user, setUser }) => {
+export const HomePage = () => {
     return (
-        <Flex flexDir="column" algin="center" justifyContent="flex-start" marginTop={["6rem","10rem"]} marginLeft={["2rem","10rem"]}>
-            <Text fontSize={"6xl"}>Welcome to Minimal</Text>
-            <Text fontSize="xl">Sometimes, less is more. Get started with Minimal today.</Text>
-            <Button variant="outline" alignSelf="flex-start" marginTop={"1rem"}>Get Started</Button>
-        </Flex>
-    );
-};
-
-export default HomePage;
+        <>
+           <Text fontSize={"6xl"}>Home Page</Text> 
+        </>
+    )
+}
